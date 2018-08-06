a041e3b36d1a1a8868c87966f6b99e148bc54b3a
shaneong <ongxx103@umn.edu>
2018-08-06 11:35:52 -0700

    azi

--- lib/Moonproject/Observation.pm
+++ lib/Moonproject/Observation.pm
@@ -1034,14 +1034,45 @@ sub getRAandDEC{
     my $self = shift;
     my $date = shift;
     
-    my ($lambda, $beta, $Delta, $epsilon) = $self->computeLongitudeLatitudeDistanceHorizonParallax($date);
+    my ($lambda, $beta, $Delta, $epsilon) = 
+    $self->computeLongitudeLatitudeDistanceHorizonParallax($date);
 
     my $T = $self->getT($date);
 
 
 
     #right ascension
-    my $RA = Astro::Coord::ECI::Utils::rad2deg(atan2(((sin(Astro::Coord::ECI::Utils::deg2rad($lambda)) * cos(Astro::Coord::ECI::Utils::deg2rad($epsilon))) - ((sin(Astro::Coord::ECI::Utils::deg2rad($beta)) / cos(Astro::Coord::ECI::Utils::deg2rad($beta))) * sin(Astro::Coord::ECI::Utils::deg2rad($epsilon)))), (cos(Astro::Coord::ECI::Utils::deg2rad($lambda)))));
+    my $RA = Astro::Coord::ECI::Utils::rad2deg(
+        atan2(
+            (
+                (sin(
+                    Astro::Coord::ECI::Utils::deg2rad(
+                        $lambda
+                    )
+                ) * cos(
+                    Astro::Coord::ECI::Utils::deg2rad(
+                        $epsilon)
+                    )
+                ) - (
+                    (sin(
+                        Astro::Coord::ECI::Utils::deg2rad(
+                            $beta)
+                        ) / cos(
+                            Astro::Coord::ECI::Utils::deg2rad(
+                                $beta)
+                            )
+                    ) * sin(
+                        Astro::Coord::ECI::Utils::deg2rad(
+                            $epsilon)
+                            )
+                    )
+            ), (cos(
+                Astro::Coord::ECI::Utils::deg2rad(
+                    $lambda)
+                    )
+                )
+        )
+    );
 
     if($RA < 0){
     	$RA = $RA + 360;
@@ -1184,9 +1215,8 @@ sub getNewElongation{
 	
     my $AZI = $self->getAZI($date);
 
-    # my $elongation = $SunHA - $MHA;
+    my $elongation = $SunHA - $AZI;
     # my $elongation = $AZI;
-     my $elongation = $self->compute_elongation_from_date($date);
 
     if ($elongation < 0){
     	$elongation = $elongation + 360;
