432e36b683f1afe273332396bcb8f6fa3e57ff5f
shaneong <ongxx103@umn.edu>
2018-08-08 11:49:08 -0700

    azi

--- lib/Moonproject/Observation.pm
+++ lib/Moonproject/Observation.pm
@@ -1135,7 +1135,7 @@ sub getHaAziAlt{
     my $long = Astro::Coord::ECI::Utils::deg2rad(93.2650);
 
     #hour angle of moon this checks out
-    my $H = $theta - 93.2650 - $RA;
+    my $H = 58.93 - 93.2650 - $RA;
 
     if ($H < 0){
     	$H = $H + 90;
@@ -1224,8 +1224,8 @@ sub getNewElongation{
     	$elongation = $elongation + 360;
     }
 
-    #return $elongation;
-    return $self->getMeanSiderealTimeGreenwich($date);
+    return $elongation;
+    #return $self->getMeanSiderealTimeGreenwich($date);
 
 }
 
