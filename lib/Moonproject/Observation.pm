610cc138bfec9ecbbc0c4249d2c6650441e1becc
shaneong <ongxx103@umn.edu>
2018-08-21 11:18:12 -0500

    gmstCCC

--- lib/Moonproject/Observation.pm
+++ lib/Moonproject/Observation.pm
@@ -1117,7 +1117,7 @@ sub getSunRA {
 
     my $SunRA = Astro::Coord::ECI::Utils::rad2deg(atan2(cos(Astro::Coord::ECI::Utils::deg2rad($L)), cos(Astro::Coord::ECI::Utils::deg2rad($e))*sin(Astro::Coord::ECI::Utils::deg2rad($L))));
 
-    return $gMod360;
+    return $SunRA;
 }
 
 #input date
