0b8f448bc77a11cbc7097736503f1fbc71f687a6
shaneong <ongxx103@umn.edu>
2018-08-21 11:44:58 -0500

    gmstCCC

--- lib/Moonproject/Observation.pm
+++ lib/Moonproject/Observation.pm
@@ -1258,7 +1258,7 @@ sub getNewElongation{
     	$elongation = -$elongation; 
     }
  
-    return $date->hour + $date->minute;
+    return $date->hour;
     #return $self->getMeanSiderealTimeGreenwich($UTDate);
     #return $RA;
 }
