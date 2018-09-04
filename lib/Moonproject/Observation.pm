edb6d6635472b2114b58f3c0ac98e399906cd134
Shane Ong <ongxx103@umn.edu>
2018-09-04 11:45:03 -0500

    change(Observation.pm): AZI calculation

--- lib/Moonproject/Observation.pm
+++ lib/Moonproject/Observation.pm
@@ -1182,11 +1182,17 @@ sub getHaAziAlt{
     #azimuthal angle
     my $AZI = Astro::Coord::ECI::Utils::rad2deg(
         atan2(
-            sin(Astro::Coord::ECI::Utils::deg2rad($H)),
-            ((cos(Astro::Coord::ECI::Utils::deg2rad($H)) * sin($lat)) - ((sin(Astro::Coord::ECI::Utils::deg2rad($DEC) / cos(Astro::Coord::ECI::Utils::deg2rad($DEC))) * cos($lat)) ) )
+
+            (cos(Astro::Coord::ECI::Utils::deg2rad($H)) * sin($lat)) - ( sin(Astro::Coord::ECI::Utils::deg2rad($DEC) / cos(Astro::Coord::ECI::Utils::deg2rad($DEC)) ) * cos($lat)),
             
-             ) );
+
+            sin(Astro::Coord::ECI::Utils::deg2rad($H))
+            
+            
+        )
+    );
     
+    $AZI = $AZI + 180; 
     #altitude
     my $altitude =  Astro::Coord::ECI::Utils::rad2deg(asin((sin($lat) * sin(Astro::Coord::ECI::Utils::deg2rad($DEC))) + (cos($lat) * cos(Astro::Coord::ECI::Utils::deg2rad($DEC)) * cos(Astro::Coord::ECI::Utils::deg2rad($H)))));
 
