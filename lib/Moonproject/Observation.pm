849ce48f8c9fb8b2adf479e02dfd9521a1c996b3
Shane Ong <ongxx103@umn.edu>
2018-09-16 19:14:58 -0500

    test querystring

--- lib/Moonproject/Observation.pm
+++ lib/Moonproject/Observation.pm
@@ -401,7 +401,7 @@ sub grade_azm {
     my $self = shift;
     my $tolMoonAZMFists = $self->get_tolerance('tolMoonAZMFists');
     return 'unknown' unless ($self->{moonHA} ne '');
-    return 'fail' unless (abs($self->{moonHA} - $self->{realAZM}) <= 10 || abs(abs($self->{moonHA} - $self->{realAZM}) - 360) <= $tolMoonAZMFists);
+    return 'fail' unless (abs($self->{moonHA} - $self->{realAZM}) <= 10 );
     return 'pass';
 }
 
