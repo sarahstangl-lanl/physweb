b9254f64741c8a755cfb116a9f97b538431f1dea
Shane Ong <ongxx103@umn.edu>
2018-09-29 11:29:38 -0500

    is_dst()

--- lib/Moonproject/Observation.pm
+++ lib/Moonproject/Observation.pm
@@ -1293,12 +1293,12 @@ sub getNewElongation{
     my $minute = $UTDate->minute;
 
     my $MHA = $self->getHA($UTDate); # June 27 2018 0930: -229.627643735931
-    if ($date->clone->set_time_zone('America/Chicago')->is_dst()) {
-        my $SunHA = (($hour + 1 + $minute/60) - 12) * 15; # June 27 2018 0930: -37.5
-    } else {
-        my $SunHA = (($hour + $minute/60) - 12) * 15; # June 27 2018 0930: -37.5
-    }
-    # my $SunHA = (($hour + $minute/60) - 12) * 15; # June 27 2018 0930: -37.5
+    # if ($date->clone->set_time_zone('America/Chicago')->is_dst()) {
+    #     my $SunHA = (($hour + 1 + $minute/60) - 12) * 15; # June 27 2018 0930: -37.5
+    # } else {
+    #     my $SunHA = (($hour + $minute/60) - 12) * 15; # June 27 2018 0930: -37.5
+    # }
+    my $SunHA = (($hour + $minute/60) - 12) * 15; # June 27 2018 0930: -37.5
 	
     my $SunRA = $self->getSunRA($UTDate);
 
